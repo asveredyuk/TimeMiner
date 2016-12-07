5e9340f1264cf4c3758137493a3c279eb32e0f4b
asveredyuk <alexsv1802@gmail.com>
2016-12-08 01:26:35 +0300

    Added log record to core

---
+++ project/Core/IntPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TimeMiner.Core
+{
+    public class IntPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public IntPoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public IntPoint()
+        {
+        }
+    }
+}
---
+++ project/Core/LogRecord.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeMiner.Core
+{
+    /// <summary>
+    /// One item for 
+    /// </summary>
+    public class LogRecord
+    {
+        /// <summary>
+        /// Time of the record
+        /// </summary>
+        public DateTime Time { get; set; }
+        /// <summary>
+        /// Time of the previous record
+        /// </summary>
+        public DateTime PreviousRecordTime { get; set; }
+        /// <summary>
+        /// Name of current foreground process
+        /// </summary>
+        public string ProcName { get; set; }
+        /// <summary>
+        /// Window title of the foreground process
+        /// </summary>
+        public string WindowTitle { get; set; }
+        /// <summary>
+        /// Mouse position at the time of record
+        /// </summary>
+        public IntPoint MousePosition { get; set; }
+        /// <summary>
+        /// Number of mouse button actions, performed after the previous record
+        /// </summary>
+        public int MouseButtonActions { get; set; }
+        /// <summary>
+        /// Number of mouse wheel actions, performed after the previous record
+        /// </summary>
+        public int MouseWheelActions { get; set; }
+        /// <summary>
+        /// Number of keys, pressed after the previous record
+        /// </summary>
+        public int Keystrokes { get; set; }
+
+    }
+}
