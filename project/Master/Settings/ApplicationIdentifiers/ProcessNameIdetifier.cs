2b7753b5571549a38fe88e35639d10e2cee403c1
asveredyuk <alexsv1802@gmail.com>
2017-03-10 15:00:41 +0300

    Added process identifier mask support

--- project/Master/Settings/ApplicationIdentifiers/ProcessNameIdetifier.cs
+++ project/Master/Settings/ApplicationIdentifiers/ProcessNameIdetifier.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TimeMiner.Core;
 
@@ -9,7 +10,21 @@ namespace TimeMiner.Master.Settings.ApplicationIdentifiers
 {
     public class ProcessNameIdetifier:ApplicationIdentifierBase
     {
-        public string ProcessName { get; set; }
+        public string ProcessName
+        {
+            get { return _processName; }
+            set
+            {
+                _processName = value;
+                if (ProcessName.Contains("*") || ProcessName.Contains("?"))
+                {
+                    regex = MakeMaskRegex(ProcessName);
+                }
+            }
+        }
+
+        private Regex regex;
+        private string _processName;
 
         public ProcessNameIdetifier()
         {
@@ -22,7 +37,22 @@ namespace TimeMiner.Master.Settings.ApplicationIdentifiers
 
         public override bool CheckRecord(LogRecord record)
         {
+            if (regex != null)
+            {
+                return CheckMaskRegex(regex, record.Process.ProcessName.ToLower());
+            }
+            //process name does not contain any special character
             return record.Process.ProcessName.Equals(ProcessName, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        private static Regex MakeMaskRegex(string mask)
+        {
+            return new Regex("^" + mask.ToLower().Replace(".", "[.]").Replace("*", ".*").Replace("?", ".") + "$", RegexOptions.Singleline);
+        }
+
+        private static bool CheckMaskRegex(Regex regex, string fname)
+        {
+            return regex.IsMatch(fname);
         }
     }
 }
