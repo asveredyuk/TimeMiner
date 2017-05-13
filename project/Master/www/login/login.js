7bffebfacaf95366b4660603f888c5033b4604b3
asveredyuk <alexsv1802@gmail.com>
2017-05-14 01:33:57 +0400

    Added styled login form
    Added styled form for login

--- project/Master/www/login/login.js
+++ project/Master/www/login/login.js
@@ -2,7 +2,7 @@
  * Created by ALEX on 13.05.2017.
  */
 $(document).ready(function () {
-    var loginTb = $("#loginInput");
+    /*var loginTb = $("#loginInput");
     var passwordTb = $("#passwordInput");
     $("#submitBt").click(function () {
         var obj = {
@@ -31,5 +31,46 @@ $(document).ready(function () {
 
 
         });
+    });*/
+    var form = $('form');
+    form.form();
+    var loginTb = form.find('input[name="login"]');
+    var passwordTb = form.find('input[name="password"]');
+    var button = form.find('.button');
+    button.click(function () {
+        button.addClass('loading');
+        var obj = {
+            Login : loginTb.val(),
+            Password:passwordTb.val()
+        };
+        var json = JSON.stringify(obj);
+        $.ajax({
+            url: "/api/login/gettoken",
+            type: 'POST',
+            data: json
+        }).done(function (msg) {
+            var message = JSON.parse(msg);
+            if(typeof message.Token != 'undefined')
+            {
+                //we got token
+                var token = message.Token;
+                Cookies.set('auth_token', token);
+                window.location = '/';//redirect
+                return;
+            }
+            if(typeof message.Error != 'undefined')
+            {
+                //got error
+                form.form('add errors', [message.Error]);
+                //loginTb.parent().parent().addClass('error');
+                //passwordTb.parent().parent().addClass('error');
+                button.removeClass('loading');
+            }
+        });
     });
+    form.submit(function () {
+        button.click();
+        return false;
+    });
+
 });
