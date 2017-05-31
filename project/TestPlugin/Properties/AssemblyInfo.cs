83c0666387ca8e40f013a433610b9a31781da422
asveredyuk <alexsv1802@gmail.com>
2017-06-01 02:31:06 +0400

    Changed test plugin and added automation
    Now builded plugin is automatically copied to plugin repository

--- project/TestPlugin/Properties/AssemblyInfo.cs
+++ project/TestPlugin/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TestPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This plugin is used to show hello world page at \" / hello\" path")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Alex Sveredyuk")]
 [assembly: AssemblyProduct("TestPlugin")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © Alex Sveredyuk 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.15.10.0")]
+[assembly: AssemblyFileVersion("0.15.10.0")]
