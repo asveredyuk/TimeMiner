6df60b72979f7fbf3a1436dbffd2e829c4e06370
asveredyuk <alexsv1802@gmail.com>
2017-06-03 01:06:37 +0400

    Added slave test plugin

---
+++ project/SlaveTestPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SlaveTestPlugin")]
+[assembly: AssemblyDescription("This plugin adds hello world item to menu and shows box")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alex Sveredyuk")]
+[assembly: AssemblyProduct("TestPlugin")]
+[assembly: AssemblyCopyright("Copyright © Alex Sveredyuk 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b47d33e-b22b-4e3a-8d48-c8bd5b52c2a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.15.15.0")]
+[assembly: AssemblyFileVersion("0.15.15.0")]
