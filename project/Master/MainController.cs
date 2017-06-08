306249f6c92f7706c9bffd39144df2c56395137b
asveredyuk <alexsv1802@gmail.com>
2017-06-08 03:21:02 +0300

    Added master startup check
    Now if failed to start - message is shown

--- project/Master/MainController.cs
+++ project/Master/MainController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using TimeMiner.Core;
 using TimeMiner.Master.Frontend;
 using TimeMiner.Master.Frontend.Plugins;
@@ -25,6 +26,7 @@ namespace TimeMiner.Master
             }
         }
 
+        private bool started;
         private FrontendServer frontendServer;
         private SlaveServer slaveServer;
         private LogsDB logsDb;
@@ -49,15 +51,45 @@ namespace TimeMiner.Master
         /// </summary>
         public void OnStartup()
         {
-            MasterPluginRepository.Self.Init();
-            slaveServer.Start();
-            frontendServer.Start();
+            if (started)
+                return;
+            try
+            {
+                MasterPluginRepository.Self.Init();
+                if(ConfigManager.Self.SlaveDataPort == ConfigManager.Self.WebInterfacePort)
+                    throw new ArgumentException("Wrong config: slave port is the same as web interface");
+                slaveServer.Start();
+                frontendServer.Start();
+                started = true;
+            }
+            catch (Exception e)
+            {
+                var res = MessageBox.Show("Application failed to start.\nEnsure that you have enough (admin) privelegies\nDisplay error info?", "Failed to start",
+                    MessageBoxButton.YesNo);
+                if (res == MessageBoxResult.Yes)
+                {
+                    string str = e.ToString();
+                    MessageBox.Show(str);
+                }
+                Application.Current.Shutdown();
+            }
         }
 
         public void OnExit()
         {
-            slaveServer.Stop();
-            frontendServer.Stop();
+            if(!started)
+                return;
+            try
+            {
+                slaveServer.Stop();
+                frontendServer.Stop();
+                started = false;
+            }
+            catch (Exception e)
+            {
+                Application.Current.Shutdown();
+            }
+            
         }
     }
 }
